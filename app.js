dbedb4dd9d5a4aae82dc21a2f407488a6819266d
wanghuan <196912334@qq.com>
2021-04-10 15:16:38 +0800

    sth

--- app.js
+++ app.js
@@ -2,6 +2,6 @@ const express = require('express')
 const app = express()
 const port = 4000
 
-app.get('/xxx', (req, res) => res.send('hello world!'))
+app.get('/xxx', (req, res) => res.send('hello 你好!'))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
